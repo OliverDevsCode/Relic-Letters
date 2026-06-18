5be9117a795aeed077cddbf182032378c4325cb4
OliverDevsCode <186149954+OliverDevsCode@users.noreply.github.com>
2026-06-18 13:14:52 +0100

    pwa support attempt 2

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -24,18 +24,42 @@ messaging.onBackgroundMessage(function(payload) {
 });
 
 
-self.addEventListener('fetch', (event) => {
-  if (!event.request.url.startsWith('http')) {
-    return; 
-  }
+const CACHE = "pwabuilder-page";
+const offlineFallbackPage = "/"; 
 
-  event.respondWith(
-    fetch(event.request).catch(() => {
-      if (event.request.mode === 'navigate') {
-        return new Response("Offline content not available", {
-          headers: { 'Content-Type': 'text/html' }
-        });
-      }
-    })
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener('install', async (event) => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then((cache) => cache.add(offlineFallbackPage))
   );
+});
+
+if (workbox.navigationPreload.isSupported()) {
+  workbox.navigationPreload.enable();
+}
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        const preloadResp = await event.preloadResponse;
+        if (preloadResp) {
+          return preloadResp;
+        }
+
+        const networkResp = await fetch(event.request);
+        return networkResp;
+      } catch (error) {
+        const cache = await caches.open(CACHE);
+        const cachedResp = await cache.match(offlineFallbackPage);
+        return cachedResp;
+      }
+    })());
+  }
 });
